714191bb7cbde5ec1498791462296f784b30bb7b
Nikolay Timofeev <nikolay.timofeev@67bricks.com>
2025-03-04 18:09:39 +0000

    Authorize the index page - allow only authenticated users to view site content

--- ProfileEditor/Controllers/HomeController.cs
+++ ProfileEditor/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using ProfileEditor.Models;
 
@@ -13,6 +14,7 @@ namespace ProfileEditor.Controllers
             _logger = logger;
         }
 
+        [Authorize]
         public IActionResult Index()
         {
             return View();
