bfd6f23e417f43557765819216b0258e70088e17
Nikolay Timofeev <nikolay.timofeev@67bricks.com>
2025-03-05 06:57:19 +0000

    Add Guard class and associated tests

---
+++ ProfileEditor/Helpers/Guard.cs
@@ -0,0 +1,19 @@
+﻿namespace ProfileEditor.Helpers {
+    public static class Guard {
+        public static T NotNull<T>(T? value, string parameterName) where T : class {
+            if (value == null) {
+                throw new ArgumentNullException(parameterName);
+            } else {
+                return value!;
+            }
+        }
+
+        public static T NotNull<T>(T? value, string parameterName) where T : struct {
+            if (!value.HasValue) {
+                throw new ArgumentNullException(parameterName);
+            } else {
+                return value.Value;
+            }
+        }
+    }
+}
---
+++ ProfileEditorTest/Helpers/GuardTests.cs
@@ -0,0 +1,58 @@
+﻿using ProfileEditor.Helpers;
+
+namespace ProfileEditorTests.Helpers {
+    public class GuardTests
+    {
+        [Fact]
+        public void NotNull_ReferenceType_WithValidValue_ReturnsValue()
+        {
+            // Arrange
+            string testValue = "test";
+
+            // Act
+            var result = Guard.NotNull(testValue, nameof(testValue));
+
+            // Assert
+            Assert.Equal(testValue, result);
+        }
+
+        [Fact]
+        public void NotNull_ReferenceType_WithNullValue_ThrowsArgumentNullException()
+        {
+            // Arrange
+            string? testValue = null;
+
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => Guard.NotNull(testValue, nameof(testValue))
+            );
+            Assert.Equal(nameof(testValue), exception.ParamName);
+        }
+
+        [Fact]
+        public void NotNull_ValueType_WithValidValue_ReturnsValue()
+        {
+            // Arrange
+            int? testValue = 42;
+
+            // Act
+            var result = Guard.NotNull(testValue, nameof(testValue));
+
+            // Assert
+            Assert.Equal(testValue.Value, result);
+        }
+
+        [Fact]
+        public void NotNull_ValueType_WithNullValue_ThrowsArgumentNullException()
+        {
+            // Arrange
+            int? testValue = null;
+
+            // Act & Assert
+            var exception = Assert.Throws<ArgumentNullException>(
+                () => Guard.NotNull(testValue, nameof(testValue))
+            );
+            Assert.Equal(nameof(testValue), exception.ParamName);
+        }
+    }
+}
